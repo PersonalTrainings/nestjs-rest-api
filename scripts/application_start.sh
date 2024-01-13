e5c4fa34afc9d6d4fdbca897d9058808053b22cd
Narek Khachatryan <nkhachat3@pmintl.net>
2024-01-13 13:32:02 +0400

    fix: update start cmd and port

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,4 +6,4 @@ pm2 delete all || echo 'nothing to delete'
 
 cd /home/ec2-user/express-app
 
-pm2 start npm -- run start:prod --name 'nest-js-api' -f -i 0
+export PORT=80; export NODE_ENV=production; pm2 start dist/main.js --name 'nest-js-api' -f -i 0
