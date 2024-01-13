bae72412723020ec8fd4ef7a793e52bbc4cbacae
Narek Khachatryan <nkhachat3@pmintl.net>
2024-01-13 13:02:11 +0400

    chore: update region and port for project deps and add pm2 start script

--- scripts/install_project_dependencies.sh
+++ scripts/install_project_dependencies.sh
@@ -16,11 +16,12 @@ npm install --legacy-peer-deps
 # Use AWS Secrets service for setting env variables in prod;
 file_location=./.env
 cat >$file_location <<EOF
-REGION="eu-central-1"
+REGION="us-east-1"
 TABLE_AUTOCREATE="true"
 TABLE_AUTOUPDATE="false"
 DEVELOPMENT="false"
 APP_NAME="Shop"
+PORT=8080
 EOF
 
 npm run build
